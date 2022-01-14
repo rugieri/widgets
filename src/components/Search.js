d103464d9dbd0712d33caac230168fb290bb3dc5
rugieri <rugieri.ronaldo@gmail.com>
2022-01-13 23:02:58 -0300

    linking to a wikipedia page

--- src/components/Search.js
+++ src/components/Search.js
@@ -20,6 +20,24 @@ const Search = () => {
     };
     search();
   }, [term]);
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -33,6 +51,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
