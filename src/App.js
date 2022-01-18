62896a7bf226a52c7d19ba606b36332fe3280d7c
rugieri <rugieri.ronaldo@gmail.com>
2022-01-17 21:38:46 -0300

    scaffolding the translate component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 const items = [
   {
@@ -36,6 +37,13 @@ const options = [
 ];
 
 const App = () => {
+  return (
+    <div>
+      <Translate />
+    </div>
+  );
+};
+/* const App = () => {
   const [selected, setSelected] = useState(options[0]);
   const [showDropdown, setShowDropdown] = useState(true);
   return (
@@ -51,7 +59,7 @@ const App = () => {
         />
       ) : null}
     </div>
-  );
-};
+  ); 
+};*/
 
 export default App;
