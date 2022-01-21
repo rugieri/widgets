aa24715c603a34222c8f6348c8452006435ec8e2
rugieri <rugieri.ronaldo@gmail.com>
2022-01-20 21:23:37 -0300

    updating the route

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    if (event.ctrlKey || event.metaKey) {
+      return;
+    }
     event.preventDefault();
+    window.history.pushState({}, '', href);
+
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   };
 
   return (
